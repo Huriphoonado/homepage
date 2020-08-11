f8b5fc14aa0e1d391615e2e94532ee8d722ead01
Willie Payne <williepain@gmail.com>
2020-08-11 17:17:07 -0400

    Fixed image resize

--- src/components/bio-card.js
+++ src/components/bio-card.js
@@ -35,9 +35,9 @@ export default function BioCard(props) {
                 <Box bg='muted' key={node.id}>
                     <Grid gap={2} columns={[ 1, 1, 2 ]}>
                         <div m={0} css={{position: "relative"}}>
-                            <Image alt={node.frontmatter.imageDescription} sx={{m: 0, p: 0}} src={node.frontmatter.image} />
+                            <Image alt={node.frontmatter.imageDescription} sx={{m: 0, p: 0, minHeight:"320px", objectFit:"cover"}} src={node.frontmatter.image} />
                             <div sx={{m: 0, p: 0}} css={{position: "absolute",  top: -6, left: 0, width: "100%", height: "100%", boxShadow: "inset 0px -70px 30px -30px rgba(0,0,0,0.7)"}} />
-                            <Styled.h2 sx={{m: 0, p: 0, pl: 4, color:"#ffff"}} css={{position: "absolute", bottom:5}}>{node.frontmatter.title}</Styled.h2>
+                            <Styled.h2 sx={{m: 0, p: 0, pl: [2,4], color:"#ffff"}} css={{position: "absolute", bottom:5}}>{node.frontmatter.title}</Styled.h2>
                         </div>
                         <Text sx={{p: 2}}>
                             <div dangerouslySetInnerHTML={{ __html: node.html }} />
