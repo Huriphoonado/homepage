1174ebd58afc95e828082636e8b1edd82df94d4d
Willie Payne <williepain@gmail.com>
2021-05-15 15:17:01 -0600

    Updates

---
+++ component---src-pages-404-js-fbd01d90745754324074.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return u}));n("q1tI");var c=n("2A+t"),o=n("izhR"),r=n("MXbp");function u(){return Object(c.c)(o.d,{sx:{textAlign:"center",py:"200px"}},Object(c.c)(r.b.h1,null,"Now you've gone too far..."),Object(c.c)(o.i,{to:"#!",href:"/"},"Get back"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-fbd01d90745754324074.js.map
