35053ac2d0d4d65164342d0cdf918aebb43f300b
Willie Payne <williepain@gmail.com>
2022-06-16 18:20:54 -0400

    Updates

---
+++ component---src-pages-404-js-31987cef85a7571b6e70.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwillie_webpage=self.webpackChunkwillie_webpage||[]).push([[883],{429:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});n(7294);var u=n(977),r=n(1877),l=n(3886);function i(){return(0,u.tZ)(r.W2,{sx:{textAlign:"center",py:"200px"}},(0,u.tZ)(l.Ge.h1,null,"Now you've gone too far..."),(0,u.tZ)(r.rU,{to:"#!",href:"/"},"Get back"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-31987cef85a7571b6e70.js.map
